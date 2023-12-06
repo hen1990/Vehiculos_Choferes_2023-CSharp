8c82d0f5c45db4c6e46e45857d01212ae0026ab6
hen1990 <hen1990@hotmail.es>
2023-12-05 22:24:13 -0300

    Agregar archivos de proyecto.

---
+++ Vehiculos_Choferes/Pages/Vehiculos/Editar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehiculos_Choferes.Pages.Vehiculos
+{
+    public class EditarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Vehiculos_Choferes/Pages/Vehiculos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehiculos_Choferes.Pages.Vehiculos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
