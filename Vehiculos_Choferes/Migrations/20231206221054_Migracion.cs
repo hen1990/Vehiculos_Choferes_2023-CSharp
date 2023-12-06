e106e716e484052def522ec8f02d32be4171025f
hen1990 <hen1990@hotmail.es>
2023-12-06 19:11:49 -0300

    Migracion

---
+++ Vehiculos_Choferes/Migrations/20231206221054_Migracion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vehiculos_Choferes.Migrations
+{
+    public partial class Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chofer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CiChofer = table.Column<int>(type: "int", nullable: false),
+                    NombreChofer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApellidoChofer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNaciminetoChofer = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chofer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Matricula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Anio = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chofer");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculo");
+        }
+    }
+}
