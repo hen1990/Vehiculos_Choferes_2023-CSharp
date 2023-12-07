2f77bdbd26c6bd43f7eed0790e27c652c5cd3088
hen1990 <hen1990@hotmail.es>
2023-12-06 21:08:34 -0300

    choferes

--- Vehiculos_Choferes/Pages/Choferes/Index.cshtml.cs
+++ Vehiculos_Choferes/Pages/Choferes/Index.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Vehiculos_Choferes.Datos;
+using Vehiculos_Choferes.Modelos;
 
 namespace Vehiculos_Choferes.Pages.Choferes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+
+        public readonly ApplicationDbContext _contexto;
+
+        public IndexModel(ApplicationDbContext contexto)
         {
+            _contexto = contexto;
+        }
+
+        public IEnumerable<Chofer> Choferes { get; set; }
+
+
+        public async Task OnGet()
+        {
+            Choferes = await _contexto.Chofer.ToListAsync();
         }
     }
 }
